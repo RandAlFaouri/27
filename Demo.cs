0a4946862c4e87f21eaa3c61fa89b0163e43fb24
RandAlFaouri <rand.alfaouri.2003@gmail.com>
2024-08-17 02:05:22 +0300

    Add files via upload

---
+++ Demo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+namespace DemoLib
+{
+    public class Demo
+    {
+        public static void Trace()
+        {
+            Console.WriteLine("Training...");
+            Console.WriteLine($"Executing Assembly: {Assembly.GetExecutingAssembly()}");
+            Console.WriteLine($"Entry Assembly: {Assembly.GetEntryAssembly()}");
+            Console.WriteLine($"Caling Assembly: {Assembly.GetCallingAssembly()}");
+           
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using DemoLib;
+using System.Reflection;
+
+namespace CAAssemblies
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            /*var type = typeof(Employee);
+            var assembly = type.Assembly;
+            Console.WriteLine(assembly.FullName);*/
+            /*var assembly= Assembly.GetExecutingAssembly();
+              Console.WriteLine(assembly.FullName);
+           Console.WriteLine(typeof(DateTime).Assembly.FullName);*/
+            // Console.WriteLine(assembly.FullName);
+           // Demo.Trace();
+           var type = typeof(Program);
+            var assembly = type.Assembly;
+          /* //3 Console.WriteLine($" Assembly Full Name: {assembly.FullName}");
+            Cosole.WriteLine($" Location Full Name: {assembly.Location}");
+            var assemblyName  = assembly.GetName();
+            Console.WriteLine($"Name: {assemblyName.Name}");
+            Console.WriteLine($"Version: {assemblyName.Version}");
+            Console.WriteLine($"Total Key Tokens length: {assemblyName.GetPublicKeyToken()}");
+            Console.WriteLine($"Code: {assemblyName.CodeBase}");
+            Console.WriteLine($"DateTime Assembly Name: {typeof(DateTime).Assembly.GetName().Name}");*/
+        }
+    }
+    class Employee
+    {
+
+    }
+}
